fde808417a716c76dea5c253feeb6a832f27cc94
Kamil Beniamin Filipek <kamil_beniamin.filipek@mailbox.tu-dresden.de>
2025-05-04 23:14:48 +0200

    Updates - full Tailwind usage for styling, reset button, new winner annotation, fixes

--- VierGewinntMultimodal/src/components/GameBoard.vue
+++ VierGewinntMultimodal/src/components/GameBoard.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { ref } from "vue";
 
-const movesCounter = ref<number>(0);
+
+const movesCounter = ref<number>(1);
 const rows = ref<number>(7);
 const columns = ref<number>(6);
 const board = ref<(null | 'red' | 'yellow')[][]>(
@@ -9,23 +10,26 @@ const board = ref<(null | 'red' | 'yellow')[][]>(
 );
 
 const currentPlayer = ref('Player 1');
+const winnerMessage = ref<string | null>(null);
 const columnHeights = ref<number[]>(Array(7).fill(0));
 
 const makeMove = (col: number) => {
-  if (columnHeights.value[col] > rows.value) return; // is the column 'full'
+  if (winnerMessage.value === null) {
+    if (columnHeights.value[col] > rows.value) return; // is the column 'full'
 
-  const row = rows.value - columnHeights.value[col] - 1; // count the row, to put the pawn into
+    const row = rows.value - columnHeights.value[col] - 1; // count the row, to put the pawn into
 
-  board.value[col][row] = currentPlayer.value === 'Player 1' ? 'yellow' : 'red'; // add a pawn
-  columnHeights.value[col] += 1;
+    board.value[col][row] = currentPlayer.value === 'Player 1' ? 'yellow' : 'red'; // add a pawn
+    columnHeights.value[col] += 1;
 
-  checkVerticalWin(col, row);
-  checkHorizontalWin(col, row);
-  checkDiagonalWinDownRight(col, row);
-  checkDiagonalWinUpRight(col, row);
+    checkVerticalWin(col, row);
+    checkHorizontalWin(col, row);
+    checkDiagonalWinDownRight(col, row);
+    checkDiagonalWinUpRight(col, row);
 
-  currentPlayer.value = currentPlayer.value === 'Player 1' ? 'Player 2' : 'Player 1';
-  movesCounter.value++;
+    currentPlayer.value = currentPlayer.value === 'Player 1' ? 'Player 2' : 'Player 1';
+    movesCounter.value++;
+  }
 };
 
 const checkVerticalWin = (col: number, row: number) => {
@@ -39,7 +43,7 @@ const checkVerticalWin = (col: number, row: number) => {
   }
 
   if(hasFourElements(result)) {
-    alert(`${currentPlayer.value} wins - 4 pawns vertically `);
+    winnerMessage.value = `${currentPlayer.value} wins - 4 pawns vertically.`;
   }
 }
 
@@ -54,7 +58,7 @@ const checkHorizontalWin = (col: number, row: number) => {
   }
 
   if(hasFourElements(result)) {
-    alert(`${currentPlayer.value} wins - 4 pawns horizontally `);
+    winnerMessage.value = `${currentPlayer.value} wins - 4 pawns horizontally.`;
   }
 }
 
@@ -72,7 +76,7 @@ const checkDiagonalWinDownRight = (col: number, row: number) => {
   }
 
   if(hasFourElements(result)) {
-    alert(`${currentPlayer.value} wins - 4 pawns diagonally \u2198`);
+    winnerMessage.value = `${currentPlayer.value} wins - 4 pawns diagonally \u2198`;
   }
 }
 
@@ -91,7 +95,7 @@ const checkDiagonalWinUpRight = (col: number, row: number) => {
   }
 
   if(hasFourElements(result)) {
-    alert(`${currentPlayer.value} wins - 4 pawns diagonally \u2197`);
+    winnerMessage.value = `${currentPlayer.value} wins - 4 pawns diagonally \u2197`;
   }
 }
 
@@ -107,14 +111,25 @@ const hasFourElements = (array: (string | null)[]) => {
   return false;
 }
 
-
-
+const resetBoard = () => {
+  board.value = Array.from({ length: columns.value }, () => Array(rows.value).fill(null));
+  columnHeights.value = Array(columns.value).fill(0);
+  currentPlayer.value = 'Player 1';
+  movesCounter.value = 1;
+  winnerMessage.value = null;
+};
 </script>
 
 <template>
-  <h2 class="text-xl text-center font-semibold mb-8">
+  <h2 v-if="!winnerMessage" class="text-xl text-center font-semibold mb-1">
     Current Player: {{ currentPlayer }}
   </h2>
+  <h2 v-else class="text-xl text-center font-semibold mb-1 text-green-600">
+    {{ winnerMessage }}
+  </h2>
+  <h5 class="text-xs text-center font-semibold mb-8">
+    Round {{ movesCounter }}
+  </h5>
   <div class="flex justify-center space-x-1">
     <div
       v-for="(col, colIndex) in board"
@@ -130,26 +145,21 @@ const hasFourElements = (array: (string | null)[]) => {
       <div
         v-for="(cell, rowIndex) in col"
         :key="rowIndex"
-        class="cell"
-        :class="cell"
+        :class="[
+          'w-[50px] h-[50px] rounded-full border border-gray-800 m-1',
+          cell === 'red' ? 'bg-red-500' : cell === 'yellow' ? 'bg-yellow-400' :'bg-gray-200'
+        ]"
       ></div>
+
     </div>
   </div>
-</template>
+  <div class="flex justify-center mt-6">
+    <button
+      @click="resetBoard"
+      class="px-6 py-2 bg-red-500 text-white rounded hover:bg-red-600 transition"
+    >
+      Reset
+    </button>
+  </div>
 
-<style>
-  .cell {
-    width: 50px;
-    height: 50px;
-    border: 1px solid #333;
-    background-color: #eee;
-    border-radius: 50%;
-    margin: 4px;
-  }
-  .cell.red {
-    background-color: red;
-  }
-  .cell.yellow {
-    background-color: yellow;
-  }
-</style>
+</template>
